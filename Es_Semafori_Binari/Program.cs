a7bf79e039e1d9b672e24634331b10e73275017c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-14 08:52:09 +0200

    Add project files.

---
+++ Es_Semafori_Binari/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Es_Semafori_Binari
+{
+    class Program
+    {
+
+        
+            static int n = 0;
+            static SemaphoreSlim s = new SemaphoreSlim(1);
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+                    n = 0;
+                    Thread t1 = new Thread(() => Incrementa());
+                    t1.Start();
+
+                    Thread t2 = new Thread(() => Decrementa());
+                    t2.Start();
+
+                    while (t1.IsAlive) { }
+
+                    while (t2.IsAlive) { }
+
+                    Console.WriteLine(n);
+
+                    Console.ReadLine();
+                }
+            }
+            private static void Decrementa()
+            {
+                for (int i = 0; i <= 1000000; i++)
+                {
+                    s.Wait();
+                    n--;
+                    s.Release();
+                }
+            }
+
+            private static void Incrementa()
+            {
+                for (int i = 0; i <= 1000000; i++)
+                {
+                    s.Wait();
+                    n++;
+                    s.Release();
+                }
+            }
+    }
+}
